81e0604dd1abcd81624111bf34c1fa0919b67c01
Hunter Moyer <120002343+hunterthenova@users.noreply.github.com>
2025-07-07 18:13:23 -0600

    Create env.js

---
+++ api/env.js
@@ -0,0 +1,10 @@
+// /api/env.js
+export default function handler(req, res) {
+  res.setHeader('Content-Type', 'application/javascript');
+  res.send(`
+    window.ENV = {
+      SUPABASE_URL: "${process.env.SUPABASE_URL}",
+      SUPABASE_ANON_KEY: "${process.env.SUPABASE_ANON_KEY}"
+    };
+  `);
+}
